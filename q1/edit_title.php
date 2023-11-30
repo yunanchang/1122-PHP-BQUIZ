c148280cc65078d16a068ece5d1a34fe07250c5f
kai <cgk0910@gmail.com>
2023-11-30 16:20:33 +0800

    完成刪除、編輯、顯示功能

--- q1/edit_title.php
+++ q1/edit_title.php
@@ -1,37 +1,17 @@
 <?php
 include_once "db.php";
-dd($_POST);
+//dd($_POST);
 foreach($_POST['id'] as $key => $id){
-    $row=$Title->find($id);
-    // dd($row);
-    $row['text']=$_POST['text'][$key];
-    // dd($row);
-    $Title->save($row);
-    
-}
-
-foreach($_POST['id'] as $id){
-    $row=$Title->find($id);
-    /* if($id==$_POST['sh']){
-        $row['sh']=1;
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $Title->del($id);
     }else{
-        $row['sh']=0;
-    } */
-    $row['sh']=($id==$_POST['sh'])?1:0;
-    $Title->save($row);
+        $row=$Title->find($id);
+        $row['text']=$_POST['text'][$key];
+        $row['sh']=($id==$_POST['sh'])?1:0;
+        $Title->save($row);
+    }
 }
-
-
-
-
-
-
-/* 
-
-$_POST['sh']=0;
-
-$Title->save($_POST);
 
 header("location:index.php");
- */
+
 ?>
