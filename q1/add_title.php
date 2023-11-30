fcbddba07950d2872ae43261570bf4e6051599e0
kai <cgk0910@gmail.com>
2023-11-30 14:23:18 +0800

    完成新增標題資料功能

---
+++ q1/add_title.php
@@ -0,0 +1,15 @@
+<?php
+include_once "db.php";
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"./img/".$_FILES['img']['name']);
+    $_POST['img']=$_FILES['img']['name'];
+}
+
+$_POST['sh']=0;
+
+$Title->save($_POST);
+
+header("location:index.php");
+
+?>
--- q1/title.php
+++ q1/title.php
@@ -1,6 +1,6 @@
 <h3 class="text-center mt-3">新增標題區圖片</h3>
 <hr>
-<form action="" method="post" enctype="multipart/form-data">
+<form action="add_title.php" method="post" enctype="multipart/form-data">
     <table class="col-8 m-auto">
         <tr>
             <td>標題區圖片:</td>
